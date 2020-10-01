8f83fe45a5b168ea4c3ef3af91e8752f37da9d09
병우 유 <conner903@naver.com>
2020-10-01 23:15:27 +0900

    add mobile env

---
+++ app.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class App extends React.Component{
+    render(){
+        return(
+            <div>
+                안녕하세요
+            </div>
+        )
+    }
+}
+
+export default App;
